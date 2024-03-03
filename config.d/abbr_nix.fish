d711aae8be4ad2033771eb080f51f7b78fb91064
kev <yqlbu@bu.edu>
2024-03-03 10:05:14 +0800

    refactor(conf.d): remove legacy nix abbrs

--- config.d/abbr_nix.fish
+++ config.d/abbr_nix.fish
@@ -1,7 +1,5 @@
 # nixos
-abbr rebuild "sudo nixos-rebuild switch --upgrade --flake .#laptop"
 abbr prefetch-git "nix-prefetch-git --url 'https://github.com/yqlbu/' --rev 'refs/heads/x1-carbon'"
 abbr clean "sudo nix-collect-garbage -d"
 abbr update "sudo nix flake update"
 abbr update-input "sudo nix flake lock --update-input"
-abbr sync-secrets "git submodule update --recursive --remote --merge"
