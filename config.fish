bd6e4068d9867e7e8bc75cd236576c4a1de1f752
kev <31861128+piyoki@users.noreply.github.com>
2025-03-19 11:10:15 -0400

    patch(config.fish): disable atuin

--- config.fish
+++ config.fish
@@ -19,7 +19,7 @@ fish_add_path $HOME/.tmux/plugins/t-smart-tmux-session-manager/bin
 source $HOME/.config/fish/functions/bangbang.fish
 source $HOME/.config/fish/functions/gnupg.fish
 source $HOME/.config/fish/functions/_fzf_gopass.fish
-source $HOME/.config/fish/functions/_fzf_atuin_history.fish
+# source $HOME/.config/fish/functions/_fzf_atuin_history.fish
 
 ### Theme ###
 source $HOME/.config/fish/themes/cool-beans-nix.fish
@@ -63,4 +63,4 @@ bind \co 'set old_tty (stty -g); stty sane; lf; stty $old_tty; commandline -f re
 zoxide init fish | source
 
 # atuin
-atuin init fish --disable-up-arrow | source
+# atuin init fish --disable-up-arrow | source
